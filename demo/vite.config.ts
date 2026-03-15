f9e75f65c1b64b3d8acbb9ebce0f3d89160c96cf
Sam P <ssaammp@gmail.com>
2026-03-15 13:09:42 -0500

    chore(demo): add hot reload for workspace dependencies

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -7,4 +7,11 @@ export default defineConfig({
     emptyOutDir: true,
   },
   publicDir: 'public',
+  resolve: {
+    alias: [
+      { find: 'like2d', replacement: '/home/sam/code/games/like2d/packages/like2d/src/index.ts' },
+      { find: 'like2d/callback', replacement: '/home/sam/code/games/like2d/packages/like2d/src/adapters/callback/index.ts' },
+      { find: 'like2d/scene', replacement: '/home/sam/code/games/like2d/packages/like2d/src/adapters/scene/index.ts' },
+    ],
+  },
 });
