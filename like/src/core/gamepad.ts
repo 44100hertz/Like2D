597f386aeb671ba28ee7ab11fd381ff960ccadbb
Sam P <ssaammp@gmail.com>
2026-03-24 11:27:41 -0500

    Remove input state tracker. Useless abstraction XD

--- like/src/core/gamepad.ts
+++ like/src/core/gamepad.ts
@@ -4,20 +4,38 @@ import { EngineDispatch } from '../engine';
 export type { LikeButton };
 
 /** A selector for a gamepad. */
-export type GamepadTarget = number;
+export type GamepadTarget = number | "any";
 
-export type ButtonTracker = {
-  prev: Set<number>,
-  current: Set<number>,
+/** Internal class: Using it externally could result
+ * in a gamepad being disconnected and still trying to maintain
+ * its state.
+ */
+class GamepadState {
+  public pressed: boolean[] = [];
+  public justPressed: boolean[] = [];
+  constructor(public index: number) {};
+
+  update(dispatch: EngineDispatch) {
+    const gp = navigator.getGamepads()[this.index]!;
+    gp.buttons.forEach((bt, i) => {
+      this.justPressed[i] = false;
+      if (bt.pressed && !this.pressed[i]) {
+        dispatch('gamepadpressed', [this.index, i, GamepadInternal.getButtonName(i)])
+        this.justPressed[i] = true;
+      } else if (!bt.pressed && this.pressed[i]) {
+        dispatch('gamepadreleased', [this.index, i, GamepadInternal.getButtonName(i)])
+      }
+      this.pressed[i] = bt.pressed;
+    });
+  }
+
+  clear() { this.pressed = [] }
 }
-
-const newButtonTracker = (): ButtonTracker =>
-  ({ prev: new Set(), current: new Set() })
 
 /** LIKE Gamepad Wrapper
  * 
  *  - Allows events/callbacks to be sent from joy buttons
- *  - Extends stateful API with justPressed
+ *  - Can track if any gamepad has a button pressed.
  * 
  * # Examples
  * 
@@ -29,74 +47,77 @@ const newButtonTracker = (): ButtonTracker =>
  * ```
  */
 export class GamepadInternal {
-  private buttonTrackers = new Map<number, ButtonTracker>();
+  private gamepads = new Map<number, GamepadState>();
   private abort = new AbortController();
 
   constructor(private dispatch: EngineDispatch) {
     // Register event listeners
-    window.addEventListener("gamepadconnected", (ev: GamepadEvent) => {
-      if (ev.gamepad.mapping == "standard") {
-        console.log(`[Gamepad] Connected standard gamepad ${ev.gamepad.id}.`);
-      } else {
+    window.addEventListener(
+      "gamepadconnected",
+      (ev: GamepadEvent) => {
+        if (ev.gamepad.mapping == "standard") {
+          console.log(`[Gamepad] Connected standard gamepad ${ev.gamepad.id}.`);
+        } else {
+          console.log(
+            `[Gamepad] Connected non-standard gamepad ${ev.gamepad.id}. Consider remapping.`,
+          );
+        }
         console.log(
-          `[Gamepad] Connected non-standard gamepad ${ev.gamepad.id}. Consider remapping.`,
+          `[Gamepad] buttons: ${ev.gamepad.buttons.length}, axes: ${ev.gamepad.axes.length}`,
         );
-      }
-      console.log(
-        `[Gamepad] buttons: ${ev.gamepad.buttons.length}, axes: ${ev.gamepad.axes.length}`,
-      );
-      this.buttonTrackers.set(ev.gamepad.index, newButtonTracker());
-    }, { signal: this.abort.signal });
-    window.addEventListener("gamepaddisconnected", (ev: GamepadEvent) => {
-      console.log(`[Gamepad] Disconnected ${ev.gamepad.id}`)
-      this.buttonTrackers.delete(ev.gamepad.index);
-    }, { signal: this.abort.signal });
-    window.addEventListener("blur", () => {
-      this.buttonTrackers.forEach(({current, prev}) => { current.clear(); prev.clear(); });
-    }, { signal: this.abort.signal });
+        this.gamepads.set(ev.gamepad.index, new GamepadState(ev.gamepad.index));
+      },
+      { signal: this.abort.signal },
+    );
+    window.addEventListener(
+      "gamepaddisconnected",
+      (ev: GamepadEvent) => {
+        console.log(`[Gamepad] Disconnected ${ev.gamepad.id}`);
+        this.gamepads.delete(ev.gamepad.index);
+      },
+      { signal: this.abort.signal },
+    );
+    window.addEventListener(
+      "blur",
+      () => {
+        this.gamepads.forEach((gps) => gps.clear());
+      },
+      { signal: this.abort.signal },
+    );
   }
 
   _update(): void {
-    const gamepads = navigator.getGamepads().filter((v) => v !== null);
-
-    gamepads.forEach((domGp, gpIndex) => {
-      const tracker = this.buttonTrackers.get(gpIndex);
-      if (!tracker) return;
-
-      const prev = tracker.current;
-      tracker.current = new Set(
-        domGp.buttons
-          .map((btn, i) => (btn.pressed ? i : null))
-          .filter((v) => v != null),
-      );
-
-      domGp.buttons.forEach((_,i) => {
-        if (i > 15) return;
-        if (tracker.prev.has(i) != tracker.current.has(i)) {
-          this.dispatch(
-            tracker.prev.has(i) ? "gamepadreleased" : "gamepadpressed",
-            [gpIndex, i, GamepadInternal.getButtonName(i)!]
-          )
-        }
-      })
-
-      tracker.prev = prev;
-    })
+    this.gamepads.forEach((gp) => gp.update(this.dispatch));
   }
 
-  isButtonDown(target: GamepadTarget, buttonRaw: number | LikeButton): boolean | undefined {
+  /** Check if a gamepad button is down. */
+  isButtonDown(
+    target: GamepadTarget,
+    buttonRaw: number | LikeButton,
+  ): boolean | undefined {
     const btn = GamepadInternal.getButtonNumber(buttonRaw);
-    return this.buttonTrackers.get(target)?.current.has(btn);
-  }
-
-  isButtonJustPressed(target: GamepadTarget, buttonRaw: LikeButton): boolean | undefined {
-    const btn = GamepadInternal.getButtonNumber(buttonRaw);
-    const bt = this.buttonTrackers.get(target);
-    if (bt) {
-      return !bt.prev.has(btn) && bt.current.has(btn);
+    if (target == "any") {
+      return this.gamepads.values().some((gp) => gp.pressed[btn]);
+    } else {
+      return this.gamepads.get(target)?.pressed[btn];
     }
   }
 
+  /** Returns true for only one frame/update if a button is pressed.
+   * Consider using `gamepadpressed` callback instead. */
+  isButtonJustPressed(
+    target: GamepadTarget,
+    buttonRaw: number | LikeButton,
+  ): boolean | undefined {
+    const btn = GamepadInternal.getButtonNumber(buttonRaw);
+    if (target == "any") {
+      return this.gamepads.values().some((gp) => gp.justPressed[btn]);
+    } else {
+      return this.gamepads.get(target)?.justPressed[btn];
+    }
+  }
+
+  /** Get a raw DOM gamepad object. */
   getGamepad(target: number): Gamepad | null {
     return navigator.getGamepads()?.[target] ?? null;
   }
@@ -116,6 +137,5 @@ export class GamepadInternal {
       ? button
       : (nameToNumber.get(button) ?? Number(button.substring(6)));
   }
-
 }
 
