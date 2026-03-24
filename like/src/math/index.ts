68bda11253cf1eae56cc3bd79e4a0bba341e3d7d
Sam P <ssaammp@gmail.com>
2026-03-23 22:22:01 -0500

    Document math module

--- like/src/math/index.ts
+++ like/src/math/index.ts
@@ -1,1 +1,16 @@
+/**
+ * @module math 
+ * 
+ * A purely functional math library, mostly for 2d.
+ * 
+ * Check out {@link vector2} and {@link rect}
+ * 
+ * Why purely functional?
+ *   1. About as fast as it can get -- the JIT will pick up on these.
+ *   2. Plays nice with stream iterators.
+ *   3. JS is likely to add a pipe operator in the future.
+ *   4. Construction is as easy as building an array.
+ */
+
+/** True modulus, conspicuously missing from JS */
 export const mod = (a: number, b: number) => ((a % b) + b) % b;
